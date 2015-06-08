415558a7426a8b3e653722350489e45375065e06
vagrant <leamerluck@gmail.com>
2015-06-08 17:49:49 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Impact::Application.config.secret_key_base = '2dc6e87a5fdec2487ffdcb27a0d017e75769528f6687450d616c7c6ca242042bbf11dea8610c915508d24076ea409fee79beeb4440cf3d38f81babb99ee4af26'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Impact::Application.config.session_store :cookie_store, key: '_impact_session'
